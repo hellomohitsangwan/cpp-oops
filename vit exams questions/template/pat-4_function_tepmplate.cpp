8eda113a5ba9afda36f826124f88176d72c77794
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-06-19 17:20:35 +0530

    qustion also added

--- vit exams questions/template/pat-4_function_tepmplate.cpp
+++ vit exams questions/template/pat-4_function_tepmplate.cpp
@@ -1,3 +1,34 @@
+
+// Develop a C++ program to find the "sum of elements in a list" using "function template" and "STL". 
+
+// For eg., create a list to hold the integer elements and take 3 integer elements as input. Same way, create an another list for floating point elements and take 3 floating point elements as input. Make a function call to perform the following.
+// Create a function template 'templist' to calculate the sum of elements in the list (for both interger and floating point elements) and display the results (elements in the list along with the sum of elements).
+
+// Note:
+// 1. Your program should use a single function template to determine the sum of elements for both integer and floating point elements.
+// 2. Your program should use the appropriate data structures "list / iterators" as required.
+ 
+
+// Example:
+// Input:
+// 10 (Integer type element belongs to the first list)
+// 30 (Integer type element belongs to the first list)
+// 50 (Integer type element belongs to the first list)
+// 1.5 (Floating point type element belongs to the second list)
+// 3.5 (Floating point type element belongs to the second list)
+// 5.5 (Floating point type element belongs to the second list)
+
+// Output:
+// 10
+// 30
+// 50
+// 90 (Sum of elements of first list)
+// 1.5
+// 3.5
+// 5.5
+// 10.5 (Sum of elements of second list)
+
+
 #include<iostream>
 #include<list>
 #include<iterator>
