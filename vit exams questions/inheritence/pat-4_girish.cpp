b4e7d6d4c31e8d41f9e89c8fbaa0f05511cc0968
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-08-29 12:12:45 +0530

    qustion statement added and new question added

---
+++ vit exams questions/inheritence/pat-4_girish.cpp
@@ -0,0 +1,107 @@
+// ques - >
+// Bugatti Car factory has four types of employees namely manager, engineer, auditor, and supervisors. The company has the following parameters to compute the salary of each employee.
+
+// Manager: Basic(Rs30000),DA(100% of basic),HRA(50% from basic),Car allowance(Rs 2500 pm),Travelling allowance(15% of basic)
+// Engineers: Basic(Rs 20000),DA(100% of basic),HRA( 30%of basic),Travelling allowance(5% of basic)
+// Auditors: Basic(Rs 35000),DA(100% of basic),HRA(30 %of basic),LTA(10% of basic, mobile allowance(rs3000),car allowance  (Rs 2500)
+// Supervisors: Basic(Rs 10000),DA(100%),HRA (25% of basic)
+ 
+
+// Create four class manager, engineer, auditor, supervisor inherited from the employee class. Employee class contains member variables name, designation, basic_salary, and a method to display salary. Derived classes contain member functions to calculate salary.
+
+ 
+
+// Input format:
+
+// John
+
+// Manager
+
+ 
+
+// Output format:
+
+// John
+
+// Manager
+
+// 82000
+
+#include <iostream>
+using namespace std;
+
+class employee {
+    public:
+    int basic_salary;
+    string name , gender , designation;
+    void put() {
+        cout<<basic_salary;
+    }
+};
+class manager : public employee{
+    public:
+    manager(string name , string des) {
+        this->name = name;
+        this->designation = des;
+    }
+    void put() {
+        //basic_salary = ; //calculate salary
+        cout<<name<<endl<<designation<<endl<<basic_salary<<endl;
+    }
+};
+class engineer : public employee{
+    public:
+        engineer(string name , string des) {
+        this->name = name;
+        this->designation = des;
+    }
+    void put() {
+        //basic_salary = ; //calculate salary
+        cout<<name<<endl<<designation<<endl<<basic_salary<<endl;
+    }
+};
+class auditor : public employee{
+    public:
+        auditor(string name , string des) {
+        this->name = name;
+        this->designation = des;
+    }
+    void put() {
+        //basic_salary = ; //calculate salary
+        cout<<name<<endl<<designation<<endl<<basic_salary<<endl;
+    }
+};
+class supervisor : public employee{
+    public:
+        supervisor(string name , string des) {
+        this->name = name;
+        this->designation = des;
+    }
+    void put() {
+        //basic_salary = ; //calculate salary
+        cout<<name<<endl<<designation<<endl<<basic_salary<<endl;
+    }
+};
+
+
+int main() {
+    string name , des; 
+    cin>>name>>des;
+    if(des == "Manager") {
+        manager m(name , des);
+        m.put();
+    }
+      else  if(des == "Engineer") {
+        engineer e(name , des);
+        e.put();
+    }
+       else if(des == "Auditor") {
+        auditor a(name , des);
+        a.put();
+    }
+       else if(des == "Supervisor") {
+        supervisor s(name , des);
+        s.put();
+    }
+
+}
--- vit exams questions/inheritence/salary_system.cpp
+++ vit exams questions/inheritence/salary_system.cpp
@@ -1,3 +1,58 @@
+
+// Write a C++ program (using inheritance) to create a payroll system for an organization with the following information.
+// Emp ID
+// Emp Name
+// Gender
+// Designation
+// Account Number
+// Account Type
+// Basic Pay, DA, HRA
+// Deductions like Provident fund, professional tax, insurance premium.
+
+// Design a base class 'employee' consisting of data members emp id, emp name, gender, designation and the member functions get_data() to get the inputs and put_data() to display the class member details. 
+
+// Create another base class 'account' consisting of data members such as account number, account type and the member functions get_info() to get the inputs and put_data() to display the class member details. 
+
+// Create a derived class 'payroll' that consists of the data members basic pay, DA, HRA and deductions (Provident fund, professional tax, insurance premium). The derived class should have the member functions: get_details() to get the neccessary inputs, 'calculate()' to perform the payroll calculations (based on the formula given below) and put_data() to display the net salary of an employee along with the employee details. 
+
+// Note:
+// Payroll calculation: 
+//     Net yearly salary = (Basic Pay+ DA + HRA) - (Provident fund + Professional tax + insurance premium).
+
+// (i) Your program should use inheritance concept.
+// (ii) Accessing the base class members by using derived class objects.
+// (iii) Resolve the ambiguity (if any) arises.
+
+// Example:
+// Input:
+// 5524 (Emp ID)
+// Ajay (Emp name)
+// male (Gender)
+// Developer (Designation)
+// 34343 (Account number)
+// savings (Account  type)
+// 300000 (Basic Pay)
+// 80000 (DA)
+// 120000 (HRA)
+// 20000 (Provident fund)
+// 6000 (Professional Tax)
+// 4000 (Insurance premium)
+
+// Output:
+// 5524 (Emp ID)
+// Ajay (Emp name)
+// male (Gender)
+// Developer (Designation)
+// 34343 (Account number)
+// savings (Account Type)
+// 300000 (Basic Pay)
+// 80000 (DA)
+// 120000 (HRA)
+// 20000 (Provident fund)
+// 6000 (Professional Tax)
+// 4000 (Insurance premium)
+// 470000 (Net Salary)
+
 #include <iostream>
 using namespace std;
 
